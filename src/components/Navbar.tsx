bf140f3b504bf08ec20c772030aacc08e90a9a73
cutugno01 <cutugnoandrea01@gmail.com>
2022-05-16 10:36:28 +0200

    navbar hover animations

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = ({
         <img className="nav-logo" src={Logo} alt="logo" />
       </div>
       <div className="nav-links">
-        <h2
+        <div
           className={`nav-link ${path[1] === "" ? "active" : ""}`}
           onClick={() => navigate(`/`)}
         >
@@ -32,8 +32,9 @@ const Navbar = ({
           >
             <path d="M448 64H192C85.96 64 0 149.1 0 256s85.96 192 192 192h256c106 0 192-85.96 192-192S554 64 448 64zM247.1 280h-32v32c0 13.2-10.78 24-23.98 24c-13.2 0-24.02-10.8-24.02-24v-32L136 279.1C122.8 279.1 111.1 269.2 111.1 256c0-13.2 10.85-24.01 24.05-24.01L167.1 232v-32c0-13.2 10.82-24 24.02-24c13.2 0 23.98 10.8 23.98 24v32h32c13.2 0 24.02 10.8 24.02 24C271.1 269.2 261.2 280 247.1 280zM431.1 344c-22.12 0-39.1-17.87-39.1-39.1s17.87-40 39.1-40s39.1 17.88 39.1 40S454.1 344 431.1 344zM495.1 248c-22.12 0-39.1-17.87-39.1-39.1s17.87-40 39.1-40c22.12 0 39.1 17.88 39.1 40S518.1 248 495.1 248z" />
           </svg>
-        </h2>
-        <h2
+          <h2 className="nav-link-name">Games</h2>
+        </div>
+        <div
           className={`nav-link ${path[1] === "search" ? "active" : ""}`}
           onClick={() => navigate("/search")}
         >
@@ -49,7 +50,8 @@ const Navbar = ({
               clipRule="evenodd"
             />
           </svg>
-        </h2>
+          <h2 className="nav-link-name">Search</h2>
+        </div>
         {/* <h2
           className={`nav-link ${path[1] === "contact" ? "active" : ""}`}
           onClick={() => navigate(`/contact`)}
